7910b1b21eaceff435320fbb0cfb335979bf7560
Rueben  van der Westhuizen <u21434809@tuks.co.za>
2025-09-19 22:28:50 +0200

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# Main file for running the project 
+# Imports 
+
+# Load data from public GitHub repository
+HYPERMARKET_URL = "https://media.githubusercontent.com/media/dsfsi/dsfsi-datasets/refs/heads/master/data/cos781/hypermarket_dataset.csv"
+
+
+def main():
+    print("Starting Load Balancing Parallel Apriori...")
+if __name__ == "__main__":
+    main()
